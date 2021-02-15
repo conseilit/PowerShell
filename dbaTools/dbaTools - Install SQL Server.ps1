ec3044faf9359dc0c08f853aa56dc708f45f1bd7
Christophe Laporte <christophe_laporte@hotmail.fr>
2021-02-15 11:13:10 +0100

    Ajout Install SQL Server

---
+++ dbaTools/dbaTools - Install SQL Server.ps1
@@ -0,0 +1,50 @@
+
+# Source reporsitory configuration
+Set-DbatoolsConfig -Name Path.SQLServerSetup -Value '\\rebond\sources'
+
+# my super secure sa password for demos
+$Username = 'sa'
+$Password = 'Password1!'
+$pass = ConvertTo-SecureString -AsPlainText $Password -Force
+$saCred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
+
+# the credential used to install SQL Server on the remote computers
+$CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+$InstallCred = Get-Credential -Message "Enter current user and password to connect remote computer" -UserName $CurrentUser
+
+# some configuration stuffs
+$config = @{
+    AGTSVCSTARTUPTYPE = "Automatic"
+    SQLCOLLATION = "Latin1_General_CI_AS"
+    BROWSERSVCSTARTUPTYPE = "Manual"
+    FILESTREAMLEVEL = 1
+    INSTALLSQLDATADIR="G:" 
+    SQLBACKUPDIR="G:\MSSQL15.MSSQLSERVER\Backup" 
+    SQLUSERDBDIR="G:\MSSQL15.MSSQLSERVER\MSSQL\Data" 
+    SQLUSERDBLOGDIR="H:\MSSQL15.MSSQLSERVER\Log" 
+    SQLTEMPDBDIR="T:\MSSQL15.MSSQLSERVER\Data" 
+    SQLTEMPDBLOGDIR="U:\MSSQL15.MSSQLSERVER\Log" 
+}
+
+# Perform the installation
+Install-DbaInstance -SqlInstance SQL15AG1,SQL15AG2 `
+                    -Credential $InstallCred `
+                    -Version 2019 `
+                    -Feature Engine,Replication,FullText,IntegrationServices `
+                    -AuthenticationMode Mixed `
+                    -AdminAccount $CurrentUser `
+                    -SaCredential $saCred `
+                    -PerformVolumeMaintenanceTasks `
+                    -SaveConfiguration C:\InstallScripts `
+                    -Configuration $config `
+                    -Confirm:$false
+
+
+# Connect using Windows authentication
+$Servers = Connect-DbaInstance -SqlInstance SQL15AG1,SQL15AG2
+$Servers | Select-Object DomainInstanceName,VersionMajor,Edition
+
+
+# Connect using SQL authentication
+$Servers = Connect-DbaInstance -SqlInstance SQL15AG1,SQL15AG2 -SqlCredential $saCred
+$Servers | get-dbaDatabase | format-table -autosize
---
+++ dbaTools/dbaTools - Install.ps1
@@ -0,0 +1,25 @@
+
+Set-ExecutionPolicy Unrestricted
+
+# Simplest way to install
+Install-Module dbatools 
+
+# Manual installation
+Invoke-WebRequest "https://github.com/sqlcollaborative/dbatools/archive/master.zip" -OutFile "C:\sources\dbatools.zip"
+Expand-Archive -Path "C:\sources\dbatools.zip" -DestinationPath "C:\Program Files\WindowsPowerShell\Modules"
+Rename-Item "C:\Program Files\WindowsPowerShell\Modules\dbatools-master"  "C:\Program Files\WindowsPowerShell\Modules\dbatools" 
+Get-ChildItem -Recurse "C:\Program Files\WindowsPowerShell\Modules\dbatools" | Unblock-File 
+import-module dbatools
+
+
+# check everything is fine
+$Server = Connect-DbaInstance -SqlInstance SQL2019
+$Server | Select-Object DomainInstanceName,VersionMajor,DatabaseEngineEdition
+
+
+
+<#
+    # issue when updating dbatools : TLS support
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+#>
+
